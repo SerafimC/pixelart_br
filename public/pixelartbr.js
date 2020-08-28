7fa4f03c2847292572b73530932dcc48e99874bd
Marcos Henrique Curtale Serafim <marcos.serafim@pampa.compasso>
2020-08-28 17:41:17 -0300

    Added on mouse down event listener do allow drawing

--- public/pixelartbr.js
+++ public/pixelartbr.js
@@ -2,6 +2,8 @@ var mesh_size = 10
 var show_mesh = true
 var color_selected = "red"
 var changes_history = []
+var mousedownID = -1; //Global ID of mouse down interval
+var mouseisdown = false
 
 function startGame() {
     drawArea.start();
@@ -120,7 +122,6 @@ function clearDraw() {
 
 function backStep() {
     drawMesh = clone_array(changes_history[changes_history.length - 1])
-    console.log(changes_history)
     changes_history.pop()
 }
 
@@ -134,7 +135,7 @@ function printOnClick(event) {
     pixel_x = Math.trunc((event.clientX - rect.left) / mesh_size)
     pixel_y = Math.trunc((event.clientY - rect.top) / mesh_size)
 
-    if ((event.clientX - rect.left) < drawArea.width && event.clientY < drawArea.height) {
+    if ((event.clientX - rect.left) < drawArea.width && (event.clientY - rect.top) < drawArea.height) {
         paint(pixel_x, pixel_y, color_selected)
     }
 
@@ -150,4 +151,54 @@ function clone_array(array) {
     return cln_arr
 }
 
+function mousedown(event) {
+    if (mousedownID == -1) { //Prevent multimple loops!
+        mousedownID = 1
+        mouseisdown = true
+    }
+}
+
+function mouseup(event) {
+    if (mousedownID != -1) { //Only stop if exists
+        clearInterval(mousedownID);
+        mousedownID = -1;
+        mouseisdown = false
+    }
+}
+document.onmousemove = handleMouseMove;
+
+function handleMouseMove(event) {
+    var dot, eventDoc, doc, body, pageX, pageY;
+
+    event = event || window.event; // IE-ism
+
+    if (event.pageX == null && event.clientX != null) {
+        eventDoc = (event.target && event.target.ownerDocument) || document;
+        doc = eventDoc.documentElement;
+        body = eventDoc.body;
+
+        event.pageX = event.clientX +
+            (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+            (doc && doc.clientLeft || body && body.clientLeft || 0);
+        event.pageY = event.clientY +
+            (doc && doc.scrollTop || body && body.scrollTop || 0) -
+            (doc && doc.clientTop || body && body.clientTop || 0);
+    }
+
+    if (mouseisdown) {
+        var rect = drawArea.canvas.getBoundingClientRect()
+        if ((event.pageX - rect.left) < drawArea.width && (event.pageY - rect.top) < drawArea.height) {
+            pixel_x = Math.trunc((event.pageX - rect.left) / mesh_size)
+            pixel_y = Math.trunc((event.pageY - rect.top) / mesh_size)
+            paint(pixel_x, pixel_y, color_selected)
+        }
+    }
+
+}
+
 document.addEventListener("click", printOnClick);
+//Assign events
+document.addEventListener("mousedown", mousedown);
+document.addEventListener("mouseup", mouseup);
+//Also clear the interval when user leaves the window with mouse
+// document.addEventListener("mouseout", mouseup);
