079ddaa103ad87f1f3d76006e6dded348afb292b
Marcos Henrique Curtale Serafim <marcos.serafim@pampa.compasso>
2020-08-29 22:14:26 -0300

    Added more colors

--- public/pixelartbr.js
+++ public/pixelartbr.js
@@ -26,6 +26,30 @@ var colorPalet = {
             }
             colorPalet_html += '</tr>'
         }
+        for (green1 = 0; green1 < 6; green1 += this.interval) {
+            colorPalet_html += '<tr>'
+            for (blue1 = 0; blue1 < 6; blue1 += this.interval) {
+                for (red1 = 0; red1 < 6; red1 += this.interval) {
+                    color_ac = hexTransl(red1) + '' + hexTransl(red1) + '' + hexTransl(green1) + '' + hexTransl(green1) + '' + hexTransl(blue1) + '' + hexTransl(blue1)
+                    console.log('.')
+                    colorPalet_html += '<td><button style="background-color: #' + color_ac + ';"' +
+                        'onclick=changeColor(' + "'#" + color_ac + "')" + '> </button></td>'
+                }
+            }
+            colorPalet_html += '</tr>'
+        }
+        for (blue1 = 0; blue1 < 6; blue1 += this.interval) {
+            colorPalet_html += '<tr>'
+            for (red1 = 0; red1 < 6; red1 += this.interval) {
+                for (green1 = 0; green1 < 6; green1 += this.interval) {
+                    color_ac = hexTransl(red1) + '' + hexTransl(red1) + '' + hexTransl(green1) + '' + hexTransl(green1) + '' + hexTransl(blue1) + '' + hexTransl(blue1)
+                    console.log('.')
+                    colorPalet_html += '<td><button style="background-color: #' + color_ac + ';"' +
+                        'onclick=changeColor(' + "'#" + color_ac + "')" + '> </button></td>'
+                }
+            }
+            colorPalet_html += '</tr>'
+        }
         colorPalet_html += '</table>'
         $('#colorpalet').html(colorPalet_html).show()
     }
